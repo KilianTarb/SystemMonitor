80ca6b6a86b8fadedb38c206befba17732b74507
Kilian <k.life00@gmail.com>
2018-06-01 19:13:52 +0800

    Added download upload speeds

--- src/components/NetworkData/InterfaceData.js
+++ src/components/NetworkData/InterfaceData.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import os from 'os';
 import util from 'os-utils';
 import si from 'systeminformation';
-import Paper from '@material-ui/core/Paper';
 
-const paperStyle = {
-	padding: "10px",
-	margin: "10px",
-	background: "#272822",
-	width: "100%"
-};
+
+var interfaces = [];
+var downstreams = [];
+var upstreams = [];
 
 class InterfaceData extends React.Component {
 	constructor(props) {
@@ -19,23 +16,48 @@ class InterfaceData extends React.Component {
 
 	componentDidMount() {
 		this.nicInit();
+		this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
 	}
 
 	componentWillUnmount() { }
 
 	nicInit() {
     si.networkInterfaces(function(data) {
+			interfaces = data;
       var elements = [];
       for (var i = 0; i < data.length; i++) {
+				// Setup the Up/Down arrays
+				upstreams.push(i);
+				downstreams.push(i);
+
         var element = React.createElement('tr', null, [
           React.createElement('td', null, data[i].iface),
-          React.createElement('td', null, data[i].ip4)
+					React.createElement('td', null, data[i].ip4),
+					React.createElement('td', {id: 'nicDown'+i}, 0),
+					React.createElement('td', {id: 'nicUp'+i}, 0)
         ]);
         elements.push(element);
       }
-      ReactDOM.render(elements, document.getElementById('nics'));
+			ReactDOM.render(elements, document.getElementById('nics'));
     });
-  }
+	}
+	
+	tick() {
+		for (var i = 0; i < interfaces.length; i++) {
+			if (!interfaces[i].internal) {
+				var downId = 'nicDown'+i;
+				var upId = 'nicUp'+i;
+				si.networkStats(interfaces[i].iface).then(data => {
+					document.getElementById(downId).innerHTML = (data.rx_sec / 1024).toFixed(0) + " KB/ps Down";
+					document.getElementById(upId).innerHTML = (data.tx_sec / 1024).toFixed(0) + " KB/ps Up";
+					console.log(data);
+				})
+			}
+		}
+	}
 
 	render() {
 		return(
