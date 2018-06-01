1739dedd450098cb0cff8dcdc48fb162fe5074aa
Kilian <k.life00@gmail.com>
2018-06-02 06:06:46 +0800

    Added networkin data chart

--- src/components/NetworkData/InterfaceData.js
+++ src/components/NetworkData/InterfaceData.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import os from 'os';
 import util from 'os-utils';
 import si from 'systeminformation';
+import {Line} from 'react-chartjs-2';
 
 
 var interfaces = [];
@@ -12,6 +13,11 @@ var upstreams = [];
 class InterfaceData extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			records: 20,
+			donwloadData: [0],
+			uploadData: [0]
+		}
 	}	
 
 	componentDidMount() {
@@ -53,22 +59,62 @@ class InterfaceData extends React.Component {
 				si.networkStats(interfaces[i].iface).then(data => {
 					document.getElementById(downId).innerHTML = (data.rx_sec / 1024).toFixed(0) + " KB/ps Down";
 					document.getElementById(upId).innerHTML = (data.tx_sec / 1024).toFixed(0) + " KB/ps Up";
-					console.log(data);
-				})
+
+					if (this.state.donwloadData.length == this.state.records) {
+						this.state.donwloadData.shift();
+						this.state.uploadData.shift();
+					}
+
+					this.state.donwloadData.push(data.rx_sec / 1024).toFixed(0);
+					this.state.uploadData.push(data.tx_sec / 1024).toFixed(0);
+					this._chart.chartInstance.update();
+				});
 			}
 		}
 	}
 
 	render() {
+		var data = {
+			labels: [
+				"1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", 
+				"11s", "12s", "13s", "14s", "15s", "16s", "17s", "18s", "19s", "20s"
+			],
+			datasets: [
+				{
+					label: 'Download',
+					data: this.state.donwloadData,
+					backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+					borderColor: ['rgba(255,99,132,1)'],
+					borderWidth: 1,
+					pointStyle: 'line'
+				}, 
+				{
+					label: 'Upload',
+					data: this.state.uploadData,
+					backgroundColor: ['rgb(118, 68, 138)'],
+					borderColor: ['rgb(175, 122, 197)'],
+					borderWidth: 2,
+					pointStyle: 'line'
+				}
+			],				
+		}
+
+		var options = {
+			maintainAspectRatio: false,
+			responsive: true
+		}
 		return(
 			<div className="data">
 				<h3>Network Interfaces</h3>
-				<table>
+				<table className="">
 					<tbody id="nics"></tbody>
 				</table>
+				<div className="">
+					<Line ref={(chart)=>{this._chart=chart}} data={data} options={options} />
+				</div>
 			</div>
 		);
 	}
-}
+}	
 
 export default InterfaceData;
