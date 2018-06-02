85df25e6e67af15ae95d810133853951af7368c1
Kilian <k.life00@gmail.com>
2018-06-03 02:14:12 +0800

    Added RTT chart

--- src/components/NetworkData/PingData.js
+++ src/components/NetworkData/PingData.js
@@ -5,17 +5,14 @@ import util from 'os-utils';
 import si from 'systeminformation';
 import Paper from '@material-ui/core/Paper';
 import ProgressProperties, { LineProperties, SemiProperties } from '../../data/ProgresProperties';
+import {Line} from 'react-chartjs-2';
 
 const ProgressBar = require('progressbar.js');
-const paperStyle = {
-	padding: "10px",
-	margin: "10px",
-	background: "#272822",
-	width: "100%"
-};
+
 
 var pingBar = null;
 var pingCode = '';
+var pingRtt = [];
 
 class PingData extends React.Component {
 	constructor(props) {
@@ -23,7 +20,8 @@ class PingData extends React.Component {
 
 		this.state = {
       pingUrl: 'http://www.google.com',
-      currentPingCode: ''
+			currentPingCode: '',
+			rttData: []
     }   
 	}	
 
@@ -46,14 +44,42 @@ class PingData extends React.Component {
 	}
 
 	ping() {
-    si.inetChecksite(this.state.pingUrl, function(data) {
+    si.inetChecksite(this.state.pingUrl).then(data => { 
       pingBar.animate(data.ms/2000);
       pingBar.setText(data.ms);
-      pingCode = data.status;
-    });
+			pingCode = data.status;
+			if (pingRtt.length > 20) {
+				pingRtt.shift();
+			} 
+			pingRtt.push(data.ms);
+			this._chart.chartInstance.update();
+		});
+		this._chart.chartInstance.update();
   }
 
 	render() {
+		var data = {
+			labels: [
+				"1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", 
+				"11s", "12s", "13s", "14s", "15s", "16s", "17s", "18s", "19s", "20s"
+			],
+			datasets: [
+				{
+					label: 'RTT',
+					data: pingRtt,
+					backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+					borderColor: ['rgba(255,99,132,1)'],
+					borderWidth: 1,
+					pointStyle: 'line'
+				}
+			],				
+		}
+
+		var options = {
+			maintainAspectRatio: false,
+			responsive: true
+		}
+
 		return(
 			<div className="data">
 				<h3>Current Round Trip Time</h3>
@@ -70,7 +96,9 @@ class PingData extends React.Component {
 					</tbody>
 				</table>
 				<hr/>
-				<div id="pingBar" className="bar">
+				<div id="pingBar" className="bar"></div>
+				<div>
+					<Line ref={(chart)=>{this._chart=chart}} data={data} options={options} />
 				</div>
 			</div>
 		);
